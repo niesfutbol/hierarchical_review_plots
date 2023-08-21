311ae02d7d9e9af301c3915f2fd2d49a51717dad
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-20 19:39:08 -0700

    🏷️ Update version tag

--- hierarchical_review_plots/__init__.py
+++ hierarchical_review_plots/__init__.py
@@ -1,6 +1,6 @@
 """Module of hierarchical review plots"""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 from .tilt_ppda_and_build_up_disruption_altair import *  # noqa
 from .polar_bar_plotly import *  # noqa
 from .heat_map_consistent_altair import *  # noqa
